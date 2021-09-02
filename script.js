62359e8f792f9ec4f08a768c0a556d9d36108bef
Nathaniel <nathjwadsworth@gmail.com>
2021-09-01 20:05:46 -0500

    Added additional quotes

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ let inspirationalQuotes = [
    If I quit, however, it will last forever.`,
    `The most important thing is this: To be able at any moment, to sacrifice 
    what you are, for what you will become!`,
-   `Everybody has a dream, but not everybody has a grind.`
+   `Everybody has a dream, but not everybody has a grind.`,
+   `Don't make a habit out of choosing what feels good over what's actually good for you.`,
+   `Done is better than perfect if perfect ain't done.`,
+   `Be phenomenal or be forgotten.`,
+   `“The only way to get out of mediocrity is to keep shooting for excellence.`,
 
 ];
 
